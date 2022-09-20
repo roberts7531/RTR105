51c01100fb25e07d275ee5f62fb89b886b469704
Roberts Jakovlevs <robe*****31@gmail.com>
2022-09-20 13:41:31 +0300

    improved git_upload script

--- git_upload.sh
+++ git_upload.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 git add .
-git commit -m "$1"
-git config --global user.email "robe*****31@gmail.com"
-git config --global user.name "Roberts Jakovlevs"
-git push https://roberts7531@github.com/roberts7531/RTR105
-
+if git commit -m "$1" ;
+then
+	git config --global user.email "robe*****31@gmail.com"
+	git config --global user.name "Roberts Jakovlevs"
+	git push https://roberts7531@github.com/roberts7531/RTR105
+fi
